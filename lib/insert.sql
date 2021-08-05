b655159ca412a25ad64010b0d066831a6c5deb3d
Daniel Suarez <suarez.daniel101@gmail.com>
2021-08-05 17:53:58 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES
+(1, "Hulky", 5, "M", "brown", "angry", 1),
+(2, "Supa", 2, "F", "tan", "mellow", 1),
+(3, "Flower", 4, "F", "blonde", "happy", 1),
+(4, "Coolio", 7, "M", "dark brown", "easygoing", 1),
+(5, "Soosoo", 10, "F", "black", "cleverly", 1),
+(6, "Lulu", 15, "F", "white", "nice", 0),
+(7, "Laughy", 1, "M", "spotted", "funny", 1),
+(8, null, 18, "M", "grey", "clumsy", 0);
+
+
